16f964052c04332a925c72ee5bee0e578b4b239f
GEEDDIGA <115898952+GEEDDIGA@users.noreply.github.com>
2025-12-04 16:35:25 +0300

    Disable SECURE_SSL_REDIRECT in production settings

--- maareeye/settings.py
+++ maareeye/settings.py
@@ -80,9 +80,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
